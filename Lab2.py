ab3de83940ed76bac0569ba8eb598406ebbf18a7
Sebastian Kristoffersson <sebkri@kth.se>
2024-01-19 16:05:27 +0100

    hj ehj

--- Lab2.py
+++ Lab2.py
@@ -6,6 +6,7 @@ class ArrayQ:
     def __init__(self):
     #     Är konstruktiorn som anropas när en ny array skapas 
     #     Parametrar : array
+            self.top = None 
             self._array= array('i') 
 
     def enqueue(self, value):  
@@ -16,27 +17,41 @@ class ArrayQ:
     def dequeue(self):
         # Metod som plockar ut det som står först i kön
         return self._array.pop(0)
+    
+    def isEmpty(self):
+        return self.top == None
+
+def test_():
+    q = ArrayQ()
+    q.enqueue(1)
+    q.enqueue(2)
+    x = q.dequeue()
+    y = q.dequeue()
+    if (x == 1 and y == 2):
+        print("OK")
+    else:
+        print("FAILED")
+
+if __name__ == '__main__':
+    order = input("Vilken ordning är korten? : ")
+    order = order.split(",")
+    for i in range(len(order)):
+        order[i] = int(order[i])   
+    #print(order)
+    q = ArrayQ()
+    a = ArrayQ()
+    for i in range(len(order)):
+       q.enqueue(order[i])
+    print(q.isEmpty() )
+    while q.isEmpty() == True: 
+        x = q.dequeue()
+        q.enqueue(x)
+        y = q.dequeue()
+        a.enqueue(y)
+    
 
 
-
-q = ArrayQ()
-q.enqueue(1)
-q.enqueue(2)
-x = q.dequeue()
-y = q.dequeue()
-
-if (x == 1 and y == 2):
-    print("OK")
-else:
-    print("FAILED")
-
-order = input("Vilken ordning är korten? : ")
-
-order = order.split(",")
-
-for i in range(len(order)):
-     order[i] = int(order[i])
+    print(q)
 
 
-
-print(order)
+    
