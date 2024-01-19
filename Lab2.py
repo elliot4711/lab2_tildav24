4e7d2732c3001aca81436e0988adb9b8a64f02e8
Sebastian Kristoffersson <sebkri@kth.se>
2024-01-19 09:55:21 +0100

    Första del av Lab 2

---
+++ Lab2.py
@@ -0,0 +1,42 @@
+from array import array 
+
+class ArrayQ:
+    # Klass för array 
+
+    def __init__(self):
+    #     Är konstruktiorn som anropas när en ny array skapas 
+    #     Parametrar : array
+            self._array= array('i') 
+
+    def enqueue(self, value):  
+        # Metod som lägger till värde längst bak
+        self._array.append(value)
+        print(self._array)
+
+    def dequeue(self):
+        # Metod som plockar ut det som står först i kön
+        return self._array.pop(0)
+
+
+
+q = ArrayQ()
+q.enqueue(1)
+q.enqueue(2)
+x = q.dequeue()
+y = q.dequeue()
+
+if (x == 1 and y == 2):
+    print("OK")
+else:
+    print("FAILED")
+
+order = input("Vilken ordning är korten? : ")
+
+order = order.split(",")
+
+for i in range(len(order)):
+     order[i] = int(order[i])
+
+
+
+print(order)
